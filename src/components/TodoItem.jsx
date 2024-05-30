80f3df51949fcc372f038a0bcc394411fe411867
wamandaka <108261088+wamandaka@users.noreply.github.com>
2024-05-30 21:40:50 +0700

    update

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -9,6 +9,11 @@ const TodoItem = ({ todo, toggleCompleted }) => {
       return { textDecoration: "none" };
     }
   };
+
+  const deleteTodo = () => {
+    console.log("deleteTodo function is called");
+  };
+
   // Definisikan function toggleCompleted di sini
   // const toggleCompleted = () => {
   //   console.log("toggleCompleted function is called");
@@ -18,10 +23,13 @@ const TodoItem = ({ todo, toggleCompleted }) => {
       <input
         type="checkbox"
         style={styles.checkbox}
-        //  Memberikan id dari todo sebagai argument
         onChange={() => toggleCompleted(todo.id)}
       />
       <p style={getTodoTitleStyle()}>{todo.title}</p>
+      {/* Tambahkan sebuah button di sini */}
+      <button onClick={deleteTodo} style={styles.button}>
+        x
+      </button>
     </div>
   );
 };
@@ -30,16 +38,25 @@ const styles = {
   todoItem: {
     border: "2px solid #f4f4f4",
     fontSize: "24px",
-    // Tambahkan styles di bawah ini
     display: "flex",
-    justifyContent: "center",
+    justifyContent: "space-between",
     alignItems: "center",
+    padding: "0 20px",
   },
-  // Tambahkan styles di bawah ini
   checkbox: {
-    marginRight: "10px",
     height: "18px",
     width: "18px",
   },
+  button: {
+    backgroundColor: "#BB0000",
+    color: "#fff",
+    height: "30px",
+    width: "30px",
+    borderRadius: "100%",
+    border: "none",
+    cursor: "pointer",
+    fontSize: "16px",
+  },
 };
+
 export default TodoItem;
